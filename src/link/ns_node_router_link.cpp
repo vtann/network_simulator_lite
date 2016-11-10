5216ef4d61c64f09a66076f520ae336462f31256
krishnk1 <karthika.krishnamurthy@mail.aalto.fi>
2016-11-10 23:24:01 +0200

    Network - Shortest Path

--- src/link/ns_node_router_link.cpp
+++ src/link/ns_node_router_link.cpp
@@ -1,11 +1,63 @@
+#ifndef __NS_NODE_ROUTER_LINK_H__
+#define __NS_NODE_ROUTER_LINK_H__
 #include "ns_node_router_link.h"
+#endif // __NS_NODE_ROUTER_LINK_H__ 
 
-int router_link::get_link_cost() const
+double router_link::get_link_cost() const
 {
     return link_cost;
 }
 
-void router_link::set_link_cost(int cost)
+void router_link::set_link_cost(double cost)
 {
     link_cost = cost;
 }
+
+void router_link::set_src_router(int src)
+{
+    src_router = src;
+}
+
+void router_link::set_dst_router(int dst)
+{
+    dst_router = dst;
+}
+
+void router_link::set_src_if(int src)
+{
+    src_interface = src;
+}
+
+void router_link::set_dst_if(int dst)
+{
+    dst_interface = dst;
+}
+
+int router_link::get_src_router() const
+{
+    return src_router;
+}
+
+int router_link::get_dst_router() const
+{
+    return dst_router;
+}
+
+int router_link::get_src_if() const
+{
+    return src_interface;
+}
+
+int router_link::get_dst_if() const
+{
+    return dst_interface;
+}
+
+void router_link::create_link(int src, int src_if, int dst, int dst_if)
+{
+    src_router = src;
+    dst_router = dst;
+    src_interface = src_if;
+    dst_interface = dst_if;
+}
+
--- src/link/ns_node_router_link.h
+++ src/link/ns_node_router_link.h
@@ -18,11 +18,35 @@ class router_link : public node_link
     
     ~router_link(); 
   
-    int get_link_cost() const;
+    double get_link_cost() const;
 
-    void set_link_cost(int cost);
+    void set_link_cost(double cost);
+
+    void set_src_router(int src);
+   
+    void set_dst_router(int dst);
+
+    void set_src_if(int src);
+ 
+    void set_dst_if(int dst);
+
+    int get_src_router() const;
+
+    int get_dst_router() const;
+
+    int get_src_if() const;
+
+    int get_dst_if() const;
+
+    void create_link(int src_router, int src_if, int dst_router, int dst_if);
+
+    //std::map < std::pair<int, int>, std::pair<int, int> > get_link() const;  
     
     private:
-    int link_cost;
-    std::map<router_interface*, router_interface*> link; 
+    //std::map < std::pair<int, int>, std::pair<int, int> > link;
+    int src_router;
+    int dst_router;
+    int src_interface;
+    int dst_interface;
+    double link_cost;
 };
