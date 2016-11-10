c2a5865d6a302f80991f1a69f43f20680f019962
krishnk1 <karthika.krishnamurthy@mail.aalto.fi>
2016-11-10 23:24:15 +0200

    Network - Shortest Path

--- src/ns_namespace.h
+++ src/ns_namespace.h
@@ -8,10 +8,26 @@ class ethr_pkt;
 
 namespace ns_ns
 {
-   const int ip_header_size = 20;  
-   const int raw_eth_header_size = 14;  
-   const int l2_ethertype = 0x0800;  
-   typedef std::vector<router_interface*> interfaces;
-   typedef std::vector<router_link*> links;
-   typedef std::vector<ethr_pkt*> packets; 
+    const int ip_header_size = 20;  
+    const int raw_eth_header_size = 14;  
+    const int l2_ethertype = 0x0800; 
+    typedef std::vector<router_interface*> interfaces;
+    typedef std::vector<router_link*> links;
+    typedef std::vector<ethr_pkt*> packets; 
+    /*enum
+    {
+        DIRECTED_GRAPH   = 0, 
+        UNDIRECTED_GRAPH
+    }graph_type; 
+    enum
+    {
+        OK     = 0,
+        NOT_OK
+    }return_type;
+
+    enum 
+    {
+        FALSE = 0,
+        TRUE = 1
+    }result;*/
 }
