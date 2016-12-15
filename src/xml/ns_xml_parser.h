223675647cdba0ed5f958d69a51ad47b6ec278fa
Tahir Irshad <tahir.irshad@aalto.fi>
2016-12-15 05:57:30 +0200

    Added comments to ns_xml_parser.h

--- src/xml/ns_xml_parser.h
+++ src/xml/ns_xml_parser.h
@@ -4,6 +4,7 @@
 #include <memory>
 #include <mutex>
 
+// dependency/library for xml parsing
 #include "tinyxml2.h"
 
 #include "../ns_namespace.h"
@@ -13,28 +14,48 @@
 using namespace tinyxml2;
 using namespace ns_ns;
 
+/**
+ * Macro for checking xml parsing error results
+ */
 #ifndef XMLCheckResult
 	#define XMLCheckResult(a_eResult) if (a_eResult != XML_SUCCESS) { printf("Error: %i\n", a_eResult); return a_eResult; }
 #endif
 
+/**
+ * Handles the parsing of an xml to retrieve the network topology
+ */
 class XMLparser{
 public:
+	/**
+	 * Constructor takes the filepath of the xml to parse
+	 */
     XMLparser(const char* filepath);
-    
+
     ~XMLparser();
-    
+
+		/**
+     * Loads the xml into memory
+     */
     int load();
 
+		/**
+     * Parses the loaded xml into the network simulator
+     */
     int process(std::shared_ptr<router_network> rn);
-    
+
+		/**
+     * extracts node and interface id from link element's source and destination string attribute
+     */
     std::vector<std::string> get_nodeAndinterfaceID(std::string param);
-    
+
+		/**
+     * Returns the filepath of the loaded xml
+     */
     std::string get_XMLfilepath() const {return this->filepath;}
-    
+
 private:
     tinyxml2::XMLDocument xmlDoc;
     tinyxml2::XMLError eResult;
     const char* filepath;
-    
-};
 
+};
